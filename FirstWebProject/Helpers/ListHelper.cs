a126238ee2fc428671c36c7ca6a65697f1ff05c1
Andrew Babenko <smaeel201356@gmail.com>
2021-09-24 17:51:48 +0300

    added helpers

---
+++ FirstWebProject/Helpers/ListHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstWebProject.Helpers
+{
+    public static class ListHelper
+    {
+        public static MvcHtmlString ShowList(this HtmlHelper html, string [] item) 
+        {
+            TagBuilder ul = new TagBuilder("ul");
+            foreach (string s in item)
+            {
+                TagBuilder li = new TagBuilder("li");
+                li.SetInnerText(s);
+                ul.InnerHtml += li.ToString();
+            }
+            return new MvcHtmlString(ul.ToString());
+        }
+    }
+}
